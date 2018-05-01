f4db3cbfe19be72b59bbd810070f81100be1bd26
dylan <1975214061@qq.com>
2018-05-01 11:34:52 +0800

    初始化工程

---
+++ src/main/java/com/dylan/cacahe/EhcacheTest.java
@@ -0,0 +1,25 @@
+package com.dylan.cacahe;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+public class EhcacheTest {
+
+    public static void main(String[] args) {
+        // 根据ehcache.xml配置文件创建Cache管理器
+        CacheManager manager = CacheManager.create("./src/main/resources/ehcache.xml");  //./src/main/resources/ehcache.xml
+        Cache c = manager.getCache("a"); // 获取指定cache
+        Element e = new Element("dylan", "牛逼");
+        c.put(e); // 把一个元素添加到Cache中
+
+        Element e2 = c.get("dylan"); // 根据key获取缓存元素
+        System.out.println(e2);
+        System.out.println(e2.getObjectValue());
+
+        c.flush(); // 刷新缓存
+        manager.shutdown(); // 关闭缓存管理器
+
+    }
+
+}
